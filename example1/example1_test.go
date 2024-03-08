cd92307883992f6cfb5358b47a772bfe46fc9f84
Leslie Dancsecs <lesdancsecs@gmail.com>
2024-03-08 11:09:00 -0400

    Had examples use _test package.

--- example1/example1_test.go
+++ example1/example1_test.go
@@ -1,9 +1,10 @@
-package example1
+package example1_test
 
 import (
 	"fmt"
 	"testing"
 
+	"github.com/dancsecs/gotomd/example1"
 	"github.com/dancsecs/sztest"
 )
 
@@ -11,10 +12,10 @@ func Test_PASS_Example1(t *testing.T) {
 	chk := sztest.CaptureNothing(t)
 	defer chk.Release()
 
-	chk.Int(TimesTwo(2), 4)
-	chk.Int(TimesThree(222222222), 666666666)
+	chk.Int(example1.TimesTwo(2), 4)
+	chk.Int(example1.TimesThree(222222222), 666666666)
 
-	structure := new(StructureType)
+	structure := new(example1.StructureType)
 	structure.F1 = "Total: "
 	chk.Str(structure.GetF1(1, 2, 3), "Total: 6")
 }
@@ -25,7 +26,7 @@ func Test_FAIL_Example1(t *testing.T) {
 
 	chk.FailFast(false) // Run all tests before exiting function.
 
-	chk.Int(TimesTwo(2), 5, "2+2=5 (is true for big values of two)")
+	chk.Int(example1.TimesTwo(2), 5, "2+2=5 (is true for big values of two)")
 	chk.Str(
 		"New in Got"+" Similar in (1) both",
 		" Similar in (2) both"+", new in Wnt",
@@ -34,7 +35,7 @@ func Test_FAIL_Example1(t *testing.T) {
 	fmt.Println("This output line will be different")
 	chk.Stdout("This output line is different")
 
-	structure := new(StructureType)
+	structure := new(example1.StructureType)
 	structure.F1 = "Total: "
 	chk.Str(structure.GetF1(1, 2, 3), "Sum: 6")
 }
